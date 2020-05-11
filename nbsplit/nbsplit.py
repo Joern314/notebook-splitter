a353e1ff11dfd12c2124cfa8564f9cdc67c63636
J.Stoehler <stub>
2020-05-11 09:16:13 +0200

    new feature: filtering lines

--- nbsplit/nbsplit.py
+++ nbsplit/nbsplit.py
@@ -10,10 +10,14 @@ import logging
 
 logger = logging.getLogger(__file__)
 
+def remove_linestarts(content, linestarts):
+    lines = [line for line in content.splitlines() if not any(line.startswith(m) for m in linestarts)]
+    return "\n".join(lines)
 
-def parse(inputfile, keep, remove, basekey):
+def parse(inputfile, keep, remove, basekey, linestarts):
     """
     From inputfile, parse the JSON and remove those cells which have values of the basekey, which are in the list of tags to remove but not in the list of tags to remove.
+    Also remove cell/line linestarts.
     """
     notebook = json.load(inputfile)
 
@@ -39,11 +43,18 @@ def parse(inputfile, keep, remove, basekey):
                         if tag not in keep:
                             keepcell = False
         if keepcell == True:
-            notebook_new["cells"].append(cell)
+            cell_new = copy.deepcopy(cell)
+            if type(cell_new['source']) is str:
+                cell_new['source'] = remove_linestarts(cell_new['source'], linestarts)
+            else: # list of string
+                cell_new['source'] = [remove_linestarts(part, linestarts) for part in cell_new['source']]
+            
+            notebook_new["cells"].append(cell_new)
         else:
             logger.info("Removing cell with JSON content:\n{}".format(json.dumps(cell, indent=4)))
 
     return notebook_new
+
 
 def main():
     parser = argparse.ArgumentParser(description=textwrap.dedent("""
@@ -57,6 +68,7 @@ def main():
     parser.add_argument('--keep',   "-k", action="append", help="Keep these tags (default: None)")
     parser.add_argument('--remove', "-r", action="append", help="Remove these tags. Special: all (remove all except for those of --keep). (default: None)")
     parser.add_argument('--basekey', type=str, help="Basekey to use for discriminating the tags (default: exercise)", default="exercise")
+    parser.add_argument('--line',   "-l", action="append", help="Remove all lines that start with these strings. (default: None)")
     parser.add_argument("-v", "--verbose", action='count', help="Verbose output; use multiple -v to increase verbosity; messages are printed to stderr (default: 0)", default=0)
     args = parser.parse_args()
     log_levels = [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG]
@@ -66,7 +78,7 @@ def main():
     sh.setLevel(log_level)
     sh.setFormatter(logging.Formatter('%(name)s::%(levelname)s::%(message)s'))
     logger.addHandler(sh)
-    notebook = parse(inputfile=args.infile, keep=args.keep, remove=args.remove, basekey=args.basekey)
+    notebook = parse(inputfile=args.infile, keep=args.keep, remove=args.remove, basekey=args.basekey, linestarts=args.line)
 
     json.dump(notebook, args.output, indent=1)
 
